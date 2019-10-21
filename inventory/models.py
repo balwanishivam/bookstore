57e0965d1814a01076cd47ed4c890d70bed35475
Shivam Balwani <shivambalwani0407@iiitvadodara.ac.in>
2019-10-21 13:20:15 +0530

    inventory initialised

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+class book(models.Model):
+    name=models.CharField(max_length=100)
+    author=models.CharField(max_length=100)
+    isbn=models.CharField(max_length=15)
 
 # Create your models here.
